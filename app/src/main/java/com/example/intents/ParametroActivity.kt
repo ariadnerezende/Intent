7f9c4d2b465b29b12538c40776a35c1d44992f7d
Ariadne <ariadnederezende@gmail.com>
2024-09-16 20:33:24 -0300

    preenchendo a segunda tela

--- app/src/main/java/com/example/intents/ParametroActivity.kt
+++ app/src/main/java/com/example/intents/ParametroActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.intents.MainActivity.Constantes.PARAMETRO_EXTRA
 import com.example.intents.databinding.ActivityParametroBinding
 
 class ParametroActivity : AppCompatActivity() {
@@ -15,6 +16,8 @@ class ParametroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(apb.root) //startActivityforResult envia uma intent,  SetResult devolve uma intent para quem chamou!
-
+        intent.getStringExtra(PARAMETRO_EXTRA)?.also { //recupera a intent que causou a abertura da tela, recupera o parametro pela chave
+            parametro -> apb.parametroEt.setText(parametro) //preenche o editText com o valor recebido
+        }
     }
 }
