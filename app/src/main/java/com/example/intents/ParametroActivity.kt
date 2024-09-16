e38bf61290a4648568eca2556cf01b035ed5a9a6
Ariadne <ariadnederezende@gmail.com>
2024-09-16 19:46:04 -0300

    apb

---
+++ app/src/main/java/com/example/intents/ParametroActivity.kt
@@ -0,0 +1,20 @@
+package com.example.intents
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.intents.databinding.ActivityParametroBinding
+
+class ParametroActivity : AppCompatActivity() {
+    private val apb: ActivityParametroBinding by lazy {
+        ActivityParametroBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(apb.root)
+
+    }
+}
