7c0fd2e515bc9d08ca5b2fcdb2d7db9a7ce580d5
Ariadne <ariadnederezende@gmail.com>
2024-09-16 22:04:41 -0300

    sem token 1

--- app/src/main/java/com/example/intents/MainActivity.kt
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.intents
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.intents.databinding.ActivityMainBinding
 
@@ -13,8 +17,9 @@ class MainActivity : AppCompatActivity() {
 
     companion object Constantes {
         const val PARAMETRO_EXTRA = "PARAMETRO_EXTRA"
-        const val PARAMETRO_REQUEST_CODE = 0 // token
     }
+
+    private lateinit var parl: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,27 +32,23 @@ class MainActivity : AppCompatActivity() {
             subtitle = this@MainActivity.javaClass.simpleName
         }
 
-        amb.entrarParametroBt.setOnClickListener {
-            val parametroIntent = Intent(this, ParametroActivity::class.java) // criar intent
-            parametroIntent.putExtra(PARAMETRO_EXTRA, amb.parametroTv.text.toString()) // chave parametro e o conteúdo
-            startActivityForResult(parametroIntent, PARAMETRO_REQUEST_CODE) // 2.chama a start com a intent e o token
-
-            // ABAIXO, O CÓDIGO MAIS NO JEITO KOTLIN.
-            // Intent(this, ParametroActivity::class.java).apply {
-            //     amb.parametroTv.text.toString().let {
-            //         putExtra(PARAMETRO_EXTRA, it)
-            //     }
-            //     startActivityForResult(this, PARAMETRO_REQUEST_CODE)
-            // }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data) //4.Tratando o retorno
-        if (requestCode == PARAMETRO_REQUEST_CODE && resultCode == RESULT_OK) {
-            data?.getStringExtra(PARAMETRO_EXTRA)?.let { retorno ->
-                amb.parametroTv.text = retorno
+        parl = registerForActivityResult( //obj, um para cada tela!
+            ActivityResultContracts.StartActivityForResult(),
+            object : ActivityResultCallback<ActivityResult> {
+                override fun onActivityResult(result: ActivityResult) {
+                    if (result.resultCode == RESULT_OK) {
+                        result.data?.getStringExtra(PARAMETRO_EXTRA)?.let {
+                            amb.parametroTv.text = it
+                        }
+                    }
+                }
             }
+        )
+
+        amb.entrarParametroBt.setOnClickListener {
+            val parametroIntent = Intent(this, ParametroActivity::class.java)
+            parametroIntent.putExtra(PARAMETRO_EXTRA, amb.parametroTv.text.toString())
+            parl.launch(parametroIntent)
         }
     }
 }
